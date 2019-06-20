5a225f70d96a48a92d72bf99906c9d93c536df80
nataliesun <natsun121@gmail.com>
2019-06-19 18:11:40 -0700

    changed DB_URL

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL:
-    process.env.DB_URL ||
+    process.env.DATABASE_URL ||
     'postgresql://estatecloud@localhost:5432/estatecloud',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
